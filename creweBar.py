de6d7efc32057fdd7f7eb63c0fd2898f9019200d
John Carney <john@pdfta.com>
2019-11-21 18:24:29 +0000

    Local tests work.

---
+++ creweBar.py
@@ -0,0 +1,20 @@
+from flask import make_response, escape
+
+
+def main(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+    """
+    request_json: dict = request.get_json()
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
---
+++ tests/test_creweBar.py
@@ -0,0 +1,33 @@
+from unittest.mock import Mock
+from creweBar import main
+
+
+def test_main_2():
+    name = 'test'
+    data = {"name": name}
+    req = Mock(get_json=Mock(return_value=data), args=data)
+
+    result = main(req)
+    assert isinstance(result, str)
+
+
+class request:
+
+    def __init__(self, json_dict: dict):
+        self.json = json_dict
+
+    def get_json(self):
+        return self.json
+
+
+def ttest_main():
+    request_json = {
+        "name": "John"
+    }
+
+    request_object = request(request_json)
+
+    response = main(request_object)
+
+    print(type(response))
+    assert isinstance(response, dict)
